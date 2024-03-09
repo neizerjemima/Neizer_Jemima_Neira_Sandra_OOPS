f03a71dc39dd4ad4c00a2b6451d3a961efb5c998
neizerjemima <113371239+neizerjemima@users.noreply.github.com>
2024-03-09 18:10:58 -0500

    still working on js

--- js/boba.js
+++ js/boba.js
@@ -1,4 +1,4 @@
-class mood {
+class Mood {
     //start a class with capital letter for best practice
     constructor(
         //Define paramaters
@@ -11,67 +11,32 @@ class mood {
         this.boba = image;
         this.quote = quote;
         this.bobaTitle = bobaTitle;
-        this.bobaTitle = bobaContent
+        this.bobaContent = bobaContent
     }
     // Add methods like normal functions:
-    mood() {
+    boBa() {
         const div = document.createElement("div");
 
         const image = document.createElement("img");
-        image.src = boba.image;
-          image.sizes = boba.imageSize;
-          image.alt = boba.name;
+        image.src = `images/${this.boba}`;
+        image.alt = this.bobaTitle; 
         div.appendChild(image);
 
         const quote = document.createElement("p");
         quote.textContent = `Don't cry drink boba and have fun`;
         div.appendChild(quote);
 
-        const Recipe = document.createElement("p");
-        Recipe.textContent = `${this.bobaTitle} <br> ${this.content}`;
-        div.body.appendChild(Recipe);
+        const recipeSteps = this.bobaContent.split(",");
+        const ul = document.createElement("ul");
+        recipeSteps.forEach(step => {
+            const li = document.createElement("li");
+            ul.appendChild(li);
+        });
+        div.appendChild(ul);
+        document.body.appendChild(div);
     }
 }
 
-//creating a one off guitar class
 
-// class BassGuitar extends Guitar {
-//     constructor() {
-//         super(
-//             "Fender",
-//             "Jazzmaster",
-//             "Black",
-//             4,
-//             "Precision Bass Pickup",
-//             "Precision Bass Pickup"
 
-//         )
-//     }
-//     SlapDaBass() {
-//         let p = document.createElement("p");
-//         p.textContent = `Slappin this ${this.brand} ${this.model}`;
-//         document.body.appendChild(p);
-//     }
-// }
-
-// class Keytar extends Guitar {
-//     constructor(
-//         brand,
-//         model,
-//         color,
-//         strings,
-//         pickupsNeck,
-//         pickupsBridge,
-//         keys    
-//     ) {
-//         super(brand, model, color, strings, pickupsNeck, pickupsBridge);
-//         this.keys = keys;
-//     }
-//     slide() {
-//         let p = document.createElement("p");
-//         p.textContent = `Sliding my finders across ${this.keys}`;
-//         document.body.appendChild(p);
-//     }
-// }
-
-export { mood };
+export { Mood };
--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-import { mood } from "./boba.js";
+import { Mood } from "./boba.js";
 
-const fenderStrat = new Guitar(
-    "Fender",
+const joy = new Mood(
+    "brownsugar.png",
     "My Happy Place is inside my Boba Cup-Happy",
     "Taro",
-    "Humbucker"
+    "1.Blend the cooked taro chunks, milk, and honey (or sugar) in a lender until smooth.,2.Add ice cubes to a glass.,3.Pour the taro milk mixture over the ice.,4.Add cooked tapioca pearls (boba) into the glass.,5.Stir gently and serve with a wide straw."
 );
 
-const gretWhiteFalcon = new Guitar(
-    "Gretsch",
+const sad = new Mood(
+    "tarro.png",
     "You can't buy happiness BUT You can buy boba - Sad",
     "Mango",
-    "Filter'Tron"
+    "1.Blend the cooked taro chunks, milk, and honey (or sugar) in a lender until smooth.,2.Add ice cubes to a glass.,3.Pour the taro milk mixture over the ice.,4.Add cooked tapioca pearls (boba) into the glass.,5.Stir gently and serve with a wide straw."
 );
 
 // const gretWhiteFalcon = new Guitar(
@@ -42,26 +42,26 @@ const gretWhiteFalcon = new Guitar(
 //     "Brown Sugar",
 //     "Filter'Tron"
 // );
-console.log(fenderStrat);
-fenderStrat.strum();
+console.log(joy);
+joy.boBa();
 
-console.log(gretWhiteFalcon);
-gretWhiteFalcon.strum();
+console.log(sad);
+sad.strum();
 
-const fenderPrecisionBass = new BassGuitar();
+// // const fenderPrecisionBass = new BassGuitar();
 
-console.log(fenderPrecisionBass);
-fenderPrecisionBass.SlapDaBass();
-fenderPrecisionBass.strum();
+// console.log(fenderPrecisionBass);
+// fenderPrecisionBass.SlapDaBass();
+// fenderPrecisionBass.strum();
 
-const alesisKeytar = new Keytar(
-    "Alesis",
-    "Vortex",
-    "Black",
-    0,
-    "None",
-    "None",
-    49
-);
-console.log(alesisKeytar);
-alesisKeytar.strum();
+// const alesisKeytar = new Keytar(
+//     "Alesis",
+//     "Vortex",
+//     "Black",
+//     0,
+//     "None",
+//     "None",
+//     49
+// );
+// console.log(alesisKeytar);
+// alesisKeytar.strum();
